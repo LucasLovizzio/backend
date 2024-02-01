af0ab7e338edc2bb396a20c87f09b9ccaaad8e25
Lucas <llovizzio@gmail.com>
2024-02-01 14:06:51 -0300

    changed to mongo atlas server

--- FastAPI/db/client.py
+++ FastAPI/db/client.py
@@ -1,3 +1,9 @@
 from pymongo import MongoClient
 
-db_client = MongoClient().local      # Si no ponemos nada en los parametros se conecta a localhost
+#Base de datos local
+#db_client = MongoClient().local      # Si no ponemos nada en los parametros se conecta a localhost
+
+#Base de datos remota
+
+db_client = MongoClient(
+    "mongodb+srv://test:test@cluster0.nbjmyvj.mongodb.net/?retryWrites=true&w=majority").test
